e0393fe7823d2f0bb246e3bf49cd7586d4ba6cec
Twigiyama <asitha@tomorrows.company>
2015-06-18 21:48:07 +0100

    First simple server function with Hello World text

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 /**
  * Created by Banzai on 18/06/15.
  */
+var http = require('http');
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hellow World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at htto://127.0.0.1:1337/');
